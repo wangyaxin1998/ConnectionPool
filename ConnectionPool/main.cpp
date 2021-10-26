7ab5b313a50cdfa2fa4d8a458ed63bac43d13e7c
wangyaxin1998 <1059299268@qq.com>
2021-10-26 21:41:45 +0800

    多线程使用连接池

--- ConnectionPool/main.cpp
+++ ConnectionPool/main.cpp
@@ -10,14 +10,23 @@ int main() {
 	*/
 	ConnectionPool* cp= ConnectionPool::getConnectionPool();
 	clock_t begin = clock();
-	int size = 1000;
-	clock_t end = clock();
+	int size = 10000;
 	vector<thread> t(4);
 	for (int i = 0; i < t.size(); ++i)
 	{
-
+		t[i] = thread([&]()
+			{
+				for (int j = 0; j < size / t.size(); ++j)
+				{
+					auto conn = cp->getConnection();
+					char sql[1024] = { 0 };
+					sprintf(sql, "insert into user(name,age,sex) values('%s','%d','%s')", "Jay", 40, "male");
+					conn->update(sql);
+				}
+			});
+		t[i].join();
 	}
-	
+	clock_t end = clock();
 	cout << (end - begin) << "ms" << endl;
 	return 0;
 }
